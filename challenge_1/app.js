0b590600e49677908d1836b05072c43b2197f45d
Anait1010 <balayashe4ka@mail.ru>
2019-02-19 14:14:53 -0800

    Working on finding winner

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,4 +1,4 @@
-{/* <td id="1a" onclick="boxClicked('1a')"></td>
+/* <td id="1a" onclick="boxClicked('1a')"></td>
 <td id="2a" onclick="boxClicked('2a')"></td>
 <td id="3a" onclick="boxClicked('3a')"></td>
 <td id="1b" onclick="boxClicked('1b')"></td>
@@ -6,43 +6,52 @@
 <td id="3b" onclick="boxClicked('3b')"></td>
 <td id="1c" onclick="boxClicked('1c')"></td>
 <td id="2c" onclick="boxClicked('2c')"></td>
-<td id="3c" onclick="boxClicked('3c')"></td> */}
+<td id="3c" onclick="boxClicked('3c')"></td> */
 
 
 
 // create 2 users with array to keep track of clicked squares
+var currentUser = 'user1';
+
 var user1 = {
-  id: 1,
   valuesPlayed: []
 };
 
 var user2 = {
-  id: 2,
   valuesPlayed: []
 };
 
-// function switchUsers(user) {
-  if(user.id === 1){
-    userMark ='O'
-  } else {
-    userMark = 'X'
-  }
-//}
-var currentUser = 'user1';
+// function switchUser() {
+//   if (currentUser === 'X') {
+//     currentUser = 'O'
+//   } else if (currentUser === 'O') {
+//     currentUser = 'X'
+//   }
+// };
+
+
+
 
 function boxClicked(position) {
 
   var userMark = '';
   
-  // if(user.id === 1){
-  //   userMark ='O'
-  // } else {
-  //   userMark = 'X'
+  if(currentUser === user1){
+    userMark = 'X';
+   // currentUser = user2;
+  } else {
+    userMark = 'O';
+  };
+  //  else if (currentUser === user2){
+  //   userMark = 'O';
+  //   currentUser = user1;
   // }
 
   document.getElementById(position).innerHTML = userMark;
 
-  var tempArray = [position[0], position[1]]
+  var tempArray = [position];
+
+  
 
   if(currentUser === "user1"){
     user1.valuesPlayed.push(tempArray)
@@ -50,18 +59,51 @@ function boxClicked(position) {
     user2.valuesPlayed.push(tempArray)
   } 
 
-//   result = isItAWin();  
-//   if(result === true){
-//     document.getElementById("winner").innerHTML = currentUser
-//   } else {
-//     //next user's move
-    
-//   }
+  console.log('tempArray ------->', tempArray);
 
+ 
+function isItAWin (){
+   var winningCombos = [['1a', '2a', '3a'], ['1b','2b', '3b'],['1c', '2c', '3c']]
+   for (var i = 0; i < winningCombos.length; i++) {
+    if(tempArray.includes(winningCombos[i])) {
+      currentUser = 'Winner';
+  }
+   }
+   if(tempArray.includes(winningCombos[0])) {
+   var winner = alert('Winner');
+   currentUser = 'winner';
   
+ 
+  }
+  //console.log('Winner');
+  // console.log(winningCombos[i]) 
+ };
+  
+   result = isItAWin();  
+  if(result === true){
+    document.getElementById("winner").innerHTML = currentUser
+  } else {
+    //next user's move
+    
+  }
 
-// }
+}
 
 // function isItAWin (){
-//   // Checks the current user's 
- };
+//    var winningCombos = [['1a', '2a', '3a'], ['1b','2b', '3b'],['1c', '2c', '3c']]
+//    for (var i = 0; i < winningCombos.length; i++) {
+//     if(tempArray.includes(winningCombos[i])) {
+//       currentUser = 'Winner';
+//   }
+//    }
+//    if(tempArray.includes(winningCombos[0])) {
+//    var winner = alert('Winner');
+//    currentUser = 'winner';
+  
+ 
+//   }
+//   //console.log('Winner');
+//   console.log(winningCombos[i]) 
+//  };
+
+ //};
