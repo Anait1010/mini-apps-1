2b9539c65be53bf92a50574ad98003bc4a306474
Anait1010 <balayashe4ka@mail.ru>
2019-02-20 15:51:42 -0800

    Finished switch users and added winning combinations

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -17,11 +17,12 @@ var user1 = {
   valuesPlayed: []
 };
 
+
 var user2 = {
   valuesPlayed: []
 };
 
-
+//function for switching users
 function switchUser() {
   if (currentUser === 'user1'){ 
     currentUser = 'user2';
@@ -40,19 +41,24 @@ function boxClicked(position) {
     userMark = 'O';
   };
   document.getElementById(position).innerHTML = userMark;
+
   switchUser();
 
-
+// temporary array that holds clicked values before it is pushed to each users array
   var tempArray = [position];
 
 //console.log('tempArray ------->', tempArray);
 
-  if(currentUser === "user1"){
+// push clicked values from tempArray to each user's array
+  if(currentUser === 'user1') {
     user1.valuesPlayed.push(tempArray)
-  } else {
+    //console.log('user array 1 ------->', user1.valuesPlayed);
+  } else  {
     user2.valuesPlayed.push(tempArray)
-  };
- //console.log('Values played for user 1 ------->', user1.valuesPlayed)
+    // console.log('user array 2 ------->', user2.valuesPlayed);
+    };
+  isItAWin(); 
+
    
 function isItAWin (){
    var winningCombos = [
@@ -60,21 +66,26 @@ function isItAWin (){
      [['1b'],['2b'], ['3b']], 
      [['1c'],['2c'], ['3c']], 
      [['1a'],['2b'], ['3c']],
-     [['3a'],['2b'], ['1c']]];
-  // console.log('Winning combos 0 ------->', winningCombos[0]);
-  //  console.log('Winning combos  1 ------->', winningCombos[1]);
+     [['3a'],['2b'], ['1c']]
+     ];
+
+     // Compare user1.valuesPlayed/user2.valuesPlayed with winningCombos[i]
+     // if they are equal we have a winner
+  
    for (var i = 0; i < winningCombos.length; i++) {
-    if(user1.valuesPlayed == winningCombos[0] || winningCombos[1]) {
-      //console.log('Winning combos ------->', winningCombos[i]);
-     user1 = 'Winner';
-    // alert('Winner')
+    //console.log('Winning combos ------->', winningCombos[i]);
+
+      if(user1.valuesPlayed.includes(winningCombos[i])) {
+        console.log('"O" is a winner');
     
-    } else if(user2.valuesPlayed.includes(winningCombos[i])) {
-    user2 = 'Winner';
-      } else  { 
-        return 'Tie'
+      } else if (user2.valuesPlayed.includes(winningCombos[i])) { 
+        console.log('"X" is a winner');
+        } else  { 
+          console.log('Tie');
       }
    };
+
+
   //  if(tempArray.includes(winningCombos[0])) {
   //  var winner = alert('Winner');
   //  currentUser = winner;
@@ -84,31 +95,13 @@ function isItAWin (){
   
  };
   
-   result = isItAWin();  
-  if(result === true){
-    document.getElementById("winner").innerHTML = currentUser
-  } else {
-    //next user's move
+  //  result = isItAWin();  
+  // if(result === true){
+  //   document.getElementById("winner").innerHTML = currentUser
+  // } else {
+  //   //next user's move
     
-  }
+  // }
 
 }
 
-// function isItAWin (){
-//    var winningCombos = [['1a', '2a', '3a'], ['1b','2b', '3b'],['1c', '2c', '3c']]
-//    for (var i = 0; i < winningCombos.length; i++) {
-//     if(tempArray.includes(winningCombos[i])) {
-//       currentUser = 'Winner';
-//   }
-//    }
-//    if(tempArray.includes(winningCombos[0])) {
-//    var winner = alert('Winner');
-//    currentUser = 'winner';
-  
- 
-//   }
-//   //console.log('Winner');
-//   console.log(winningCombos[i]) 
-//  };
-
- //};
