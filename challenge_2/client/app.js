95dcac362769b84f42d49701fd96be8a04bb00a6
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-02-23 14:46:14 -0800

    Implemented client side code: post request and

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,18 @@
+$(document).ready(function(){
+  
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    var myForm  = document.getElementById('form');
+    var formData = new FormData(myForm);
 
+    $.ajax({
+      type: "POST",
+      url: '/fileData',
+      data: formData,
+      success: function(data) {
+        $('body').html(data);
+      }, //catch the data that server sends
+    });
+  });   
+});
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,3 +1,34 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'client')));
+
+
+app.post('/fileData',function (req, res) {
+
+
+var template = 
+`
+<body>
+    <h1> CSV Report Generator </h1>
+      <form id = 'form' method = 'POST' action = '/fileData'> 
+        <textarea name = 'text' placeholder='Enter CSV data'></textarea>
+        <input type ='submit' value='Submit'>
+      </form>
+  </body>
+`
+//console.log('template', template)
+res.send(template);
+res.end();
+});
+
+
+
+//catches post request from html
+app.listen(8080, function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
