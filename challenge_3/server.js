5d136c8fa5b22b04753bd8e00f5177a25bf4cba9
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-02-25 21:24:32 -0800

    Installed package.json

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var path = require('path');
 
 
+
+app.use(express.join(path.join(__dirname, 'client')));
+app.listen(8080, function(err) {
+  if(err) {
+    console.log(err);
+  }
+})
