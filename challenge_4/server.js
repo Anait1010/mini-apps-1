66973840b2303296bcabbab99a85eebabd85d354
ANAIT <Anait@ANAITs-MacBook-Pro.local>
2019-03-04 17:58:39 -0800

    Working on server and client side

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,21 @@
+var express = require('express');
+var app  = express();
+var path = require('path');
+var bodyParser = require('body-parser');
 
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.json());
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname,'/public/index.html'))
+});
+
+
+
+app.listen(8080, function(err) {
+  if (err) {
+    console.log('error', err);
+  }
+});
 
